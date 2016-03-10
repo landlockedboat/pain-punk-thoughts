e53eeddd5f8724da1fc5f8168e1cf09441e32331
wextia <valcazar96@gmail.com>
2016-03-10 23:12:18 +0100

    yo yo yo this is getting ridicolous

--- server.js
+++ server.js
@@ -4,3 +4,29 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 server.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
+
+var TwitterBot = require("node-twitterbot").TwitterBot
+var WordsDB = require("./database.js").Database
+
+//WordsDB = new Database();
+
+var PunkPainBot = new TwitterBot({
+  "consumer_secret" :
+  "5GQvdOD4fG1wbTWasYlEAK05xaC2avtrT7AifYE2hJ9cIJbues",
+  "consumer_key" :
+  "gtxRo4j3Fcmcu9Okne28oIep4",
+  "access_token" :
+  "708020567778136064-CXfARagXYiaMi5Hc11iJ0Uh87WuqDUH",
+  "access_token_secret" :
+  "lsEAi8qxGaZUbqTxB3D6BhaZszqBtc3I0VIzYWKEdb8dV"
+});
+
+function TweetAThought(){
+    var tweetContent = WordsDB.GetRandomHitWord() +
+    " me in the " + WordsDB.GetRandomBodyPartWord() +
+    " with a " + WordsDB.GetRandomMeleeWord();
+    console.log(tweetContent);
+    PunkPainBot.tweet(tweetContent);
+}
+TweetAThought();
+setInterval(TweetAThought, 1000 * 60 * 30);
